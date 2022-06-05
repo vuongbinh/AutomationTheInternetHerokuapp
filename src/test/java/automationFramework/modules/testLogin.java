064828f1c6ff607d6d72de83d8bb219846cb12d6
Vuong Binh <binh.vuonghao@gmail.com>
2022-06-05 13:37:32 +0700

    Create test case for login error

--- src/test/java/automationFramework/modules/testLogin.java
+++ src/test/java/automationFramework/modules/testLogin.java
@@ -1,45 +1,57 @@
 package automationFramework.modules;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import org.testng.Assert;
 import automationFramework.pages.Login_Page;
 import automationFramework.supporter.ExcelUltils;
 
 public class testLogin extends Test_Controller {
-        @DataProvider(name = "hybridDATA")
-        public static Object[][] getDataFromProvider() throws IOException {
-                Object[][] object;
-                ExcelUltils file = new ExcelUltils();
-
-                Sheet excelSheet = file.readFile("src/main/resources", "data_LoginTest.xlsx", "Sheet1");
-                int rowCOUNT = excelSheet.getLastRowNum() - excelSheet.getFirstRowNum();
-                object = new Object[rowCOUNT][2];
-                for (int i = 0; i < rowCOUNT; i++) {
-                        Row row = excelSheet.getRow(i + 1);
-                        for (int j = 0; j < row.getLastCellNum(); j++) {
-                                object[i][j] = row.getCell(j).toString();
-                        }
-                }
-                System.out.println();
-                return object;
-        }
-
-        // Here we are calling the Data Provider object with its Name
+        // Verify login success
         @Test(dataProvider = "hybridDATA")
-        public void test(String usr, String pwd) {
+        public void verify_LoginSuccess(String usr, String pwd) {
                 Login_Page login_Page = new Login_Page(driver);
                 login_Page.open();
                 login_Page.login(usr, pwd);
                 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                Assert.assertEquals(driver.findElement(By.xpath("//*[@id='userName-value']")).getText(),usr);
+                Assert.assertEquals(driver.findElement(By.xpath("//*[@id='userName-value']")).getText(), usr);
                 Assert.assertTrue(driver.findElement(By.xpath("//*[.='Log out']")).isDisplayed());
                 login_Page.logout();
         }
+
+        // Verify login error msg
+        @Test(dataProvider = "hybridDATA")
+        public void verify_LoginError(String usr, String pwd) {
+                Login_Page login_Page = new Login_Page(driver);
+                login_Page.open();
+                login_Page.login(usr, pwd);
+                wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.id("name")));
+                Assert.assertEquals(driver.findElement(By.id("name")).getText(), "Invalid username or password!");
+                Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+        }
+
+        @DataProvider(name = "hybridDATA")
+        public static Object[][] getDataFromProvider(Method method) throws IOException {
+                ExcelUltils file = new ExcelUltils();
+                if (method.getName() == "verify_LoginSuccess") {
+                        Sheet excelSheet = file.readFile("src/main/resources", "data_LoginTest.xlsx",
+                                        "valid_Account");
+                        getDataFromFile(excelSheet);
+                } else if (method.getName() == "verify_LoginError") {
+                        Sheet excelSheet = file.readFile("src/main/resources", "data_LoginTest.xlsx",
+                                        "invalid_Account");
+                        getDataFromFile(excelSheet);
+                }
+                return null;
+        }
+
 }
