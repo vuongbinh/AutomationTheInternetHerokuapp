3d777543c66b5209dfcfdca78c13e881c616e34e
Vuong Binh <binh.vuonghao@gmail.com>
2022-06-15 00:27:08 +0700

    TEST PLAN CREATED

--- src/test/java/automationFramework/pages/Login_Page.java
+++ src/test/java/automationFramework/pages/Login_Page.java
@@ -2,6 +2,10 @@ package automationFramework.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Login_Page extends Page_Controller {
 
@@ -9,6 +13,7 @@ public class Login_Page extends Page_Controller {
     By username = By.id("userName");
     By password = By.id("password");
     By logoutBTN = By.xpath("//*[.='Log out']");
+    WebDriverWait wait;
 
     public Login_Page(WebDriver driver) {
         super(driver);
@@ -20,12 +25,16 @@ public class Login_Page extends Page_Controller {
     }
 
     public void login(String usr, String pwd) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable((loginButton)));
         driver.findElement(username).sendKeys(usr);
         driver.findElement(password).sendKeys(pwd);
         driver.findElement(loginButton).click();
     }
 
     public void logout() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable((logoutBTN)));
         driver.findElement(logoutBTN).click();
     }
 
